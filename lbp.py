72c924bb303e24cea9a02282f050e53e42a51673
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-08-01 15:30:39 +0430

    Create lbp.py

---
+++ lbp.py
@@ -0,0 +1,30 @@
+# lbp calculation for an image.
+# example usage: 
+# gray = cv2.imread('image.jpg', cv2.IMREAD_GRAY)
+# descriptor = LocalBinaryPatterns(24, 8)
+# lbp = descriptor.describe(gray)
+
+
+from skimage.feature import local_binary_pattern
+import numpy as np
+
+class LocalBinaryPatterns:
+    def __init__(self, numPoints, radius):
+        # store the number of points and radius
+        self.numPoints = numPoints
+        self.radius = radius
+
+    def describe(self, image, eps=1e-7):
+        # compute the Local Binary Pattern representation
+        # of the image, and then use the LBP representation
+        # to build the histogram of patterns
+        lbp = local_binary_pattern(image, self.numPoints,
+                                   self.radius, method="uniform")
+        (hist, _) = np.histogram(lbp.ravel(),
+                                 bins=np.arange(0, self.numPoints + 3),
+                                 range=(0, self.numPoints + 2))
+        # normalize the histogram
+        hist = hist.astype("float")
+        hist /= (hist.sum() + eps)
+        # return the histogram of Local Binary Patterns
+        return hist
